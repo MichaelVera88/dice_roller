bdebe89facd24c51a137322ef54baa9a80d18e57
Mike <143658803+MichaelVera88@users.noreply.github.com>
2024-06-28 11:57:44 -0400

    Theme Options in Settings
    Created an option within the settings menu to turn the program theme into either light, dark, or their current system.

--- gui/gui.py
+++ gui/gui.py
@@ -4,22 +4,26 @@
 import customtkinter
 
 
+
 ### Window
 # -------------------------------------------------
 
 # Set light/dark theme based on system
-theme = customtkinter.set_appearance_mode("System")
+system_theme = customtkinter.set_appearance_mode("System")
+light_theme = customtkinter.set_appearance_mode("Light")
+dark_theme = customtkinter.set_appearance_mode("Dark")
 
-# Create window
+# Create Window
 window = customtkinter.CTk()
 
-# Set window title
+# Window Title
 window.title("Dice Roller")
 
-# Set minimum window size
+# Minimum Window Size
 window.minsize(600, 500)
 
 # -------------------------------------------------
+
 
 
 ### Settings
@@ -36,14 +40,39 @@ def settings_icon_click():
 	else:
 		settings_menu.grid(row=0, column=1, padx=15, pady=15, sticky="nw")
 
-# Create an icon for settings
+
+def settings_theme_click(value):
+	"""
+	Settings Theme Click Function:
+
+	Sets the theme of the program to either light or dark; defaults to system on startup
+	"""
+	if value == "System":
+		customtkinter.set_appearance_mode("System")
+	elif value == "Light":
+		customtkinter.set_appearance_mode("Light")
+	elif value == "Dark":
+		customtkinter.set_appearance_mode("Dark")
+		
+		
+
+# Settings Icon
 settings_icon = customtkinter.CTkButton(window, text="Settings", width=50, height=25, command=settings_icon_click)
 settings_icon.grid(row=0, column=0, padx=15, pady=15, sticky="nw")
 
-# Create settings menu frame
-settings_menu = customtkinter.CTkFrame(window, width=150, height=250, fg_color=theme)
+# Settings Menu Frame
+settings_menu = customtkinter.CTkFrame(window, fg_color=system_theme)
 
-# -------------------------------------------------
+# Light/Dark Theme Settings Option 
+option_theme_label = customtkinter.CTkLabel(settings_menu, text="Theme Color:")
+option_theme_label.grid(row=0, column=0, padx=15, pady=15, sticky='n')
+
+option_theme_button = customtkinter.CTkSegmentedButton(settings_menu, values=["System", "Light", "Dark"], command=settings_theme_click)
+option_theme_button.grid(row=0, column=1, padx=15, pady=15, sticky="n")
+option_theme_button.set("System")
+
+
+
 
 # Run Window
 window.mainloop()
