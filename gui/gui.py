7791279596caf3c68c4c139981bf801829499cda
Mike <143658803+MichaelVera88@users.noreply.github.com>
2024-07-02 13:15:01 -0400

    Main Window Class
    Created main window class in gui.py and opened it in main.py

--- gui/gui.py
+++ gui/gui.py
@@ -1,11 +1,15 @@
 ### Creates the main window and GUI for program
 
-### Importing custom tkinter to get graphical user interface tools
-import customtkinter
-### Importing Image from Pillow to properly get icons
+import customtkinter as ctk
 from PIL import Image
 
-
+# Main Window
+class Window(ctk.CTk):
+	def __init__(self):
+		super().__init__()
+		self.title("Dice Roller")
+		self.resizable(False, False)
+		self.geometry("500x500")
 
 if __name__ == "__main__":
 	print("Window file")
--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 ### Main program file
 
-### Import graphical user interface file
-import gui.gui
-### Import dice usage
-from src.dice import Dice
+from gui.gui import *
 
-
-
-d20 = Dice(20)
-
-roll = d20.roll(2, True)
-
-print(f"{roll[0]}{roll[1]}")
-
+dice_roller = Window()
+dice_roller.mainloop()
    
